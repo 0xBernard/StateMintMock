e6b8843725b176a545c809636c27a36ca85290fd
0xBernard <bowtiedbernard@proton.me>
2025-06-30 12:05:32 -0700

    Turned back on server side rendering

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  trailingSlash: true,
   images: {
-    unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/StateMintMock' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/StateMintMock/' : '',
 };
 
 export default nextConfig;
